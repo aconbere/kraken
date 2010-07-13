15500c3a802c02c4adba58d92943aab025c14ca0
Anders Conbere <aconbere@gmail.com>
2010-07-13 16:15:37 -0700

    updates

--- Makefile
+++ Makefile
@@ -1,8 +1,9 @@
 .PHONY : build
 build :
 		echo "Building Kraken."
-		mkdir -p "build"
-		cat lib/kraken.js lib/kraken/* > "build/kraken.js"
+		mkdir -p build
+		rm build/kraken.js
+		cat lib/kraken.js lib/kraken/* > build/kraken.js
 		echo "Done, see build/kraken.js"
 
 .PHONY : clean
--- examples/todo/app/controllers/todo_form_controller.js
+++ examples/todo/app/controllers/todo_form_controller.js
@@ -1,7 +1,7 @@
 Todo.controllers.TodoForm = function (context) {
   console.log("Starting: controllers.TodoForm");
 
-  this.form = new context.views.TodoForm("#todo-form", this);
+  this.form = new Todo.views.TodoForm("#todo-form", this);
 
   this.form.bind("submit", function (value) {
     var item = new Todo.models.Item({"value": value});
