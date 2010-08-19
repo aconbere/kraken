3b1ddbe8d57116c11ff184d77ec27b7f09d64736
Anders Conbere <aconbere@gmail.com>
2010-08-19 09:31:31 -0700

    moving all application state onto the context

--- examples/todo/app/todo.js
+++ examples/todo/app/todo.js
@@ -1,1 +1,9 @@
-var Todo = new Kraken.Core();
+Controller1.register("view1", View1);
+Controller1.register("view2", View2);
+
+var options = { "controllers": [ Controller1
+                               , Controller2
+                               , Controller3
+                               ]}
+
+var Todo = new Kraken.Core(options);
